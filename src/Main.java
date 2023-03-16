83043ac587254569c59e626676d1635566e1bbc3
Abhijeet <abhijeet.chkrbrt7@gmail.com>
2023-03-16 21:38:11 +0530

    Test project

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,16 @@
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        var list = List.of("Ram", "Shyam", "Mohan", "Ravi");
-        var lists = List.of("");
+        var list = List.of("Ram", "Shyam", "Mohan", "Ravi", "Ram");
+        //var lists = List.of("");
         System.out.println(list.stream().findFirst().get());
         System.out.println("First element Using reduce --> " + list.stream().reduce((f, s) -> s));
         System.out.println("Last element Using skip --> " + list.stream().skip(list.size() - 1).findFirst());
+        System.out.println("Finding duplicate ele-> " + list.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream().filter(entry -> entry.getValue() > 1).collect(Collectors.toList()));
     }
 }
 
